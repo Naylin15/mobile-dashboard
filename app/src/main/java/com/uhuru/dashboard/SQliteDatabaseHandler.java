74c7dc3f2d74fdc818f711516f79a9e9ba7e83aa
Naylin <nmedina@teclib.com>
2017-11-28 17:27:30 -0400

    docs(header): add license header to source code

--- app/src/main/java/com/uhuru/dashboard/SQliteDatabaseHandler.java
+++ app/src/main/java/com/uhuru/dashboard/SQliteDatabaseHandler.java
@@ -1,3 +1,27 @@
+/**
+ * ----------------------------------------------------------------
+ *  LICENSE
+ *
+ *  This file is part of the Uhuru Mobile Dashboard for Android,
+ *  a subproject of Uhuru Mobile.
+ *
+ *  Uhuru Mobile is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  Uhuru Mobile is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  ----------------------------------------------------------------
+ *  @copyright (C) 2017 Teclib' and contributors.
+ *  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ *  @link      https://github.com/uhuru-mobile/mobile-dashboard
+ *  @link      http://www.uhuru-mobile.com/
+ *  ----------------------------------------------------------------
+ */
+
 package com.uhuru.dashboard;
 
 import android.content.ContentValues;
